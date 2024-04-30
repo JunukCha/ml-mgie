fd2bca946f266f22c3b6f6f92836a3a79b198cb1
Junuk Cha <jucha@unist.ac.kr>
2024-04-29 17:02:44 -0700

    demo

---
+++ CustomDataset.py
@@ -0,0 +1,35 @@
+import os
+import json
+import pdb
+import sys
+import numpy as np
+import torch
+from PIL import Image
+import glob
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self):
+        self.source_list = glob.glob("../test/images/source/*.jpg")
+        self.target_list = glob.glob("../test/images/target/*.jpg")
+        self.text_list = glob.glob("../test/text/*.txt")
+        self.source_list.sort()
+        self.target_list.sort()
+        self.text_list.sort()
+
+    def __len__(self):
+        return 100 # len(self.text_list)
+
+    def __getitem__(self, i):
+        with open(self.text_list[i], "r") as f:
+            prompt = f.read().splitlines()[0]
+        image_0 = Image.open(self.source_list[i])
+        image_1 = Image.open(self.target_list[i])
+        image_0 = np.array(image_0.resize((600, 600)))
+        image_1 = np.array(image_1.resize((600, 600)))
+        image_0 = torch.FloatTensor(image_0)/255
+        image_1 = torch.FloatTensor(image_1)/255
+        image_0 = image_0.permute(2, 0, 1)
+        image_1 = image_1.permute(2, 0, 1)
+        return image_0, image_1, prompt
--- demo.py
+++ demo.py
@@ -6,9 +6,13 @@ from PIL import Image
 
 import torch as T
 import transformers, diffusers
+from torch.data.utils import DataLoader
+from torchvision.utils import save_image
 
+from CustomDataset import CustomDataset
 from llava.conversation import conv_templates
 from llava.model import *
+
 
 def crop_resize(f, sz=512):
     w, h = f.size
@@ -30,73 +34,77 @@ def remove_alter(s):  # hack expressive instruction
     return s.strip()
 
 
-DEFAULT_IMAGE_TOKEN = '<image>'
-DEFAULT_IMAGE_PATCH_TOKEN = '<im_patch>'
-DEFAULT_IM_START_TOKEN = '<im_start>'
-DEFAULT_IM_END_TOKEN = '<im_end>'
-PATH_LLAVA = './_ckpt/LLaVA-7B-v1'
+def main():
+    DEFAULT_IMAGE_TOKEN = '<image>'
+    DEFAULT_IMAGE_PATCH_TOKEN = '<im_patch>'
+    DEFAULT_IM_START_TOKEN = '<im_start>'
+    DEFAULT_IM_END_TOKEN = '<im_end>'
+    PATH_LLAVA = './_ckpt/LLaVA-7B-v1'
 
-tokenizer = transformers.AutoTokenizer.from_pretrained(PATH_LLAVA)
-model = LlavaLlamaForCausalLM.from_pretrained(PATH_LLAVA, low_cpu_mem_usage=True, torch_dtype=T.float16, use_cache=True).cuda()
-image_processor = transformers.CLIPImageProcessor.from_pretrained(model.config.mm_vision_tower, torch_dtype=T.float16)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(PATH_LLAVA)
+    model = LlavaLlamaForCausalLM.from_pretrained(PATH_LLAVA, low_cpu_mem_usage=True, torch_dtype=T.float16, use_cache=True).cuda()
+    image_processor = transformers.CLIPImageProcessor.from_pretrained(model.config.mm_vision_tower, torch_dtype=T.float16)
 
-tokenizer.padding_side = 'left'
-tokenizer.add_tokens(['[IMG0]', '[IMG1]', '[IMG2]', '[IMG3]', '[IMG4]', '[IMG5]', '[IMG6]', '[IMG7]'], special_tokens=True)
-model.resize_token_embeddings(len(tokenizer))
-ckpt = T.load('./_ckpt/mgie_7b/mllm.pt', map_location='cpu')
-model.load_state_dict(ckpt, strict=False)
+    tokenizer.padding_side = 'left'
+    tokenizer.add_tokens(['[IMG0]', '[IMG1]', '[IMG2]', '[IMG3]', '[IMG4]', '[IMG5]', '[IMG6]', '[IMG7]'], special_tokens=True)
+    model.resize_token_embeddings(len(tokenizer))
+    ckpt = T.load('./_ckpt/mgie_7b/mllm.pt', map_location='cpu')
+    model.load_state_dict(ckpt, strict=False)
 
-mm_use_im_start_end = getattr(model.config, 'mm_use_im_start_end', False)
-tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
-if mm_use_im_start_end: tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
+    mm_use_im_start_end = getattr(model.config, 'mm_use_im_start_end', False)
+    tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
+    if mm_use_im_start_end: tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
 
-vision_tower = model.get_model().vision_tower[0]
-vision_tower = transformers.CLIPVisionModel.from_pretrained(vision_tower.config._name_or_path, torch_dtype=T.float16, low_cpu_mem_usage=True).cuda()
-model.get_model().vision_tower[0] = vision_tower
-vision_config = vision_tower.config
-vision_config.im_patch_token = tokenizer.convert_tokens_to_ids([DEFAULT_IMAGE_PATCH_TOKEN])[0]
-vision_config.use_im_start_end = mm_use_im_start_end
-if mm_use_im_start_end: vision_config.im_start_token, vision_config.im_end_token = tokenizer.convert_tokens_to_ids([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN])
-image_token_len = (vision_config.image_size//vision_config.patch_size)**2
+    vision_tower = model.get_model().vision_tower[0]
+    vision_tower = transformers.CLIPVisionModel.from_pretrained(vision_tower.config._name_or_path, torch_dtype=T.float16, low_cpu_mem_usage=True).cuda()
+    model.get_model().vision_tower[0] = vision_tower
+    vision_config = vision_tower.config
+    vision_config.im_patch_token = tokenizer.convert_tokens_to_ids([DEFAULT_IMAGE_PATCH_TOKEN])[0]
+    vision_config.use_im_start_end = mm_use_im_start_end
+    if mm_use_im_start_end: vision_config.im_start_token, vision_config.im_end_token = tokenizer.convert_tokens_to_ids([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN])
+    image_token_len = (vision_config.image_size//vision_config.patch_size)**2
 
-_ = model.eval()
-EMB = ckpt['emb'].cuda()
-with T.inference_mode(): NULL = model.edit_head(T.zeros(1, 8, 4096).half().to('cuda'), EMB)
-print('NULL:', NULL.shape)
+    _ = model.eval()
+    EMB = ckpt['emb'].cuda()
+    with T.inference_mode(): NULL = model.edit_head(T.zeros(1, 8, 4096).half().to('cuda'), EMB)
+    print('NULL:', NULL.shape)
 
-pipe = diffusers.StableDiffusionInstructPix2PixPipeline.from_pretrained('timbrooks/instruct-pix2pix', torch_dtype=T.float16, safety_checker=None).to('cuda')
-pipe.set_progress_bar_config(disable=True)
-pipe.unet.load_state_dict(T.load('./_ckpt/mgie_7b/unet.pt', map_location='cpu'))
+    pipe = diffusers.StableDiffusionInstructPix2PixPipeline.from_pretrained('timbrooks/instruct-pix2pix', torch_dtype=T.float16, safety_checker=None).to('cuda')
+    pipe.set_progress_bar_config(disable=True)
+    pipe.unet.load_state_dict(T.load('./_ckpt/mgie_7b/unet.pt', map_location='cpu'))
 
-SEED = 13331
+    SEED = 13331
 
-ins = ['make the frame red', 'turn the day into night', 'give him a beard', 'make cottage a mansion', 
-       'remove yellow object from dogs paws', 'change the hair from red to blue', 'remove the text', 'increase the image contrast', 
-       'remove the people in the background', 'please make this photo professional looking', 'darken the image, sharpen it', 'photoshop the girl out', 
-       'make more brightness', 'take away the brown filter form the image', 'add more contrast to simulate more light', 'dark on rgb', 
-       'make the face happy', 'change view as ocean', 'replace basketball with soccer ball', 'let the floor be made of wood']
-for i in tqdm(range(len(ins))):
-    img, txt = Image.open('_input/%d.jpg'%(i)).convert('RGB'), ins[i]
+    dataset = CustomDataset()
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
     
-    img = image_processor.preprocess(img, return_tensors='pt')['pixel_values'][0]
-    txt = "what will this image be like if '%s'"%(txt)
-    txt = txt+'\n'+DEFAULT_IM_START_TOKEN+DEFAULT_IMAGE_PATCH_TOKEN*image_token_len+DEFAULT_IM_END_TOKEN
-    conv = conv_templates['vicuna_v1_1'].copy()
-    conv.append_message(conv.roles[0], txt), conv.append_message(conv.roles[1], None)
-    txt = conv.get_prompt()
-    txt = tokenizer(txt)
-    txt, mask = T.as_tensor(txt['input_ids']), T.as_tensor(txt['attention_mask'])
-    
-    with T.inference_mode():
-        out = model.generate(txt.unsqueeze(dim=0).cuda(), images=img.half().unsqueeze(dim=0).cuda(), attention_mask=mask.unsqueeze(dim=0).cuda(), 
-                             do_sample=False, max_new_tokens=96, num_beams=1, no_repeat_ngram_size=3, 
-                             return_dict_in_generate=True, output_hidden_states=True)
-        out, hid = out['sequences'][0].tolist(), T.cat([x[-1] for x in out['hidden_states']], dim=1)[0]
+    for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+        img_x, img_y, txt = data
         
-        p = min(out.index(32003)-1 if 32003 in out else len(hid)-9, len(hid)-9)
-        hid = hid[p:p+8]
+        img = image_processor.preprocess(img_x, return_tensors='pt')['pixel_values'][0]
+        txt = "what will this image be like if '%s'"%(txt)
+        txt = txt+'\n'+DEFAULT_IM_START_TOKEN+DEFAULT_IMAGE_PATCH_TOKEN*image_token_len+DEFAULT_IM_END_TOKEN
+        conv = conv_templates['vicuna_v1_1'].copy()
+        conv.append_message(conv.roles[0], txt), conv.append_message(conv.roles[1], None)
+        txt = conv.get_prompt()
+        txt = tokenizer(txt)
+        txt, mask = T.as_tensor(txt['input_ids']), T.as_tensor(txt['attention_mask'])
+        
+        with T.inference_mode():
+            out = model.generate(txt.unsqueeze(dim=0).cuda(), images=img.half().unsqueeze(dim=0).cuda(), attention_mask=mask.unsqueeze(dim=0).cuda(), 
+                                do_sample=False, max_new_tokens=96, num_beams=1, no_repeat_ngram_size=3, 
+                                return_dict_in_generate=True, output_hidden_states=True)
+            out, hid = out['sequences'][0].tolist(), T.cat([x[-1] for x in out['hidden_states']], dim=1)[0]
+            
+            p = min(out.index(32003)-1 if 32003 in out else len(hid)-9, len(hid)-9)
+            hid = hid[p:p+8]
 
-        out = remove_alter(tokenizer.decode(out))
-        emb = model.edit_head(hid.unsqueeze(dim=0), EMB)
-        res = pipe(image=Image.open('_input/%d.jpg'%(i)).convert('RGB'), prompt_embeds=emb, negative_prompt_embeds=NULL, generator=T.Generator(device='cuda').manual_seed(SEED)).images[0]
-    
+            out = remove_alter(tokenizer.decode(out))
+            emb = model.edit_head(hid.unsqueeze(dim=0), EMB)
+            res = pipe(image=Image.open('_input/%d.jpg'%(i)).convert('RGB'), prompt_embeds=emb, negative_prompt_embeds=NULL, generator=T.Generator(device='cuda').manual_seed(SEED)).images[0]
+
+        results_folder = f"results/{i:03d}"
+        os.makedirs(results_folder, exist_ok=True)
+        save_image(img_x[0], os.path.join(results_folder, "input.jpg"))
+        save_image(img_y[0], os.path.join(results_folder, "target.jpg"))
+        save_image(res, os.path.join(results_folder, "pred.jpg"))
