a9873bad1b97976011f38f88bf33bbb5294f3486
Junuk Cha <jucha@unist.ac.kr>
2024-04-29 21:12:32 -0700

    Create make_zip_eval_llava_ft.py

---
+++ make_zip_eval_llava_ft.py
@@ -0,0 +1,14 @@
+import os, os.path as osp
+import zipfile
+import glob
+
+zip_path = 'mgie.zip'
+
+files_to_zip = glob.glob(osp.join("results", "*", "concatenated_image.jpg"))
+files_to_zip.sort()
+
+with zipfile.ZipFile(zip_path, 'w') as zipf:
+    for file in files_to_zip[75:100]:
+        zipf.write(file, arcname=file)
+        
+print("ZIP file created successfully!")
