b179832f14a653d858b5178bf30eab15f7a3e199
Junuk Cha <jucha@unist.ac.kr>
2024-05-02 16:13:16 -0700

    Update CustomDataset.py

--- CustomDataset.py
+++ CustomDataset.py
@@ -4,7 +4,8 @@ import pdb
 import sys
 import numpy as np
 import torch
-from PIL import Image
+from PIL import Image, ImageOps
+import math
 import glob
 from torch.utils.data import Dataset
 
@@ -34,3 +35,29 @@ class CustomDataset(Dataset):
         image_0 = image_0.permute(2, 0, 1)
         image_1 = image_1.permute(2, 0, 1)
         return image_0, image_1, prompt
+    
+class CustomDatasetHQ(Dataset):
+    def __init__(self):
+        self.source_list = glob.glob("../../code/demo_output/*/source.jpg")
+        self.text_list = glob.glob("../../code/demo_output/*/text.txt")
+        self.source_list.sort()
+        self.text_list.sort()
+
+    def __len__(self):
+        return 100 # len(self.text_list)
+
+    def __getitem__(self, i):
+        with open(self.text_list[i], "r") as f:
+            prompt = f.read().splitlines()[0]
+        img_x = Image.open(self.source_list[i])
+        width, height = img_x.size
+        factor = 512 / max(width, height)
+        factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
+        width = int((width * factor) // 64) * 64
+        height = int((height * factor) // 64) * 64
+        img_x = ImageOps.fit(img_x, (width, height), method=Image.Resampling.LANCZOS)
+        
+        image_0 = np.array(img_x.resize((512, 512)))
+        image_0 = torch.FloatTensor(image_0)/255
+        image_0 = image_0.permute(2, 0, 1)
+        return image_0, prompt
