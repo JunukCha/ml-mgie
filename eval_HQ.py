93069126c6a9b8dbc92d80559499be29c94320cd
Junuk Cha <jucha@unist.ac.kr>
2024-05-11 17:38:55 -0700

    Update eval_HQ.py

--- eval_HQ.py
+++ eval_HQ.py
@@ -135,7 +135,7 @@ def main():
     lpips_list = []
     for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
         img_x, target, txt = data
-        target = Image.fromarray(target)
+        target = Image.fromarray(target.cpu().numpy()[0])
         txt = txt[0]
         
         img = image_processor.preprocess(img_x, return_tensors='pt')['pixel_values'][0]
